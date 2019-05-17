dbe3e3035b098ebe7c82150a6aed602fa8e5c5b4
md054-Nitish <nitish.kaila@multidots.com>
2019-05-17 16:45:33 +0530

    Initial commit

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
 /**
- * Index File 12125
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
  * @package WordPress
  */
 
-$abc = "abcdddddfsdfsfd";
-$a = wp_is_mobile();
-echo 'Hello World!!!!!';
-echo 'Hello World!!!!!';
-echo 'sfsdffgfg';
-echo 'dkdkdfdf';
-echo "abcd46464"; // phpcs:ignore
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
