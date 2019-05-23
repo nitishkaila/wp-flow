3930abb8df10915a28c1883e55ecb541472d2c45
md054-Nitish <nitish.kaila@multidots.com>
2019-05-23 17:44:33 +0530

    change

--- wp-content/themes/twentynineteen/test.php
+++ wp-content/themes/twentynineteen/test.php
@@ -1,9 +1,8 @@
 <?php
 
-
+//$abc = extract($name);
 $name = filter_input( INPUT_POST, 'name', FILTER_SANITIZE_STRING );
 ?>
 <a href="<?php echo esc_url($name);?>">test</a>
 <?php
 
-//$abc = extract($name);
