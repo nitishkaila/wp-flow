8c497d10d9f919353088548e400a08cc8edbf5c9
md054-Nitish <nitish.kaila@multidots.com>
2019-05-23 17:34:28 +0530

    change

--- wp-content/themes/twentynineteen/test.php
+++ wp-content/themes/twentynineteen/test.php
@@ -5,21 +5,4 @@ $name = filter_input( INPUT_POST, 'name', FILTER_SANITIZE_STRING );
 ?>
 <a href="<?php echo esc_url($name);?>">test</a>
 <?php
-$args = array(
-    'post_type' => 'post',
-    'posts_per_page' => -1
-);
-
-$my_query = new WP_Query( $args );
-
-if ( $my_query->have_posts() ) {
-
-    while ( $my_query->have_posts() ) {
-
-        $my_query->the_post();
-
-        // Post data goes here.
-
-    }
-
-}
+$dd = get_posts();
