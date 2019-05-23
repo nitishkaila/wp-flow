dc6c625bb48b39f2b760ccbf0e4ad5ec8f37ee08
md054-Nitish <nitish.kaila@multidots.com>
2019-05-23 17:29:16 +0530

    change

--- wp-content/themes/twentynineteen/test.php
+++ wp-content/themes/twentynineteen/test.php
@@ -7,5 +7,19 @@ $name = filter_input( INPUT_POST, 'name', FILTER_SANITIZE_STRING );
 <?php
 $args = array(
     'post_type' => 'post',
-    'posts_per_page' => -1
+    'posts_per_page' => 150
 );
+
+$my_query = new WP_Query( $args );
+
+if ( $my_query->have_posts() ) {
+
+    while ( $my_query->have_posts() ) {
+
+        $my_query->the_post();
+
+        // Post data goes here.
+
+    }
+
+}
