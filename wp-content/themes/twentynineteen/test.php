9ad76d49c1ce446d0edad9ed665ebb051485843d
md054-Nitish <nitish.kaila@multidots.com>
2019-05-23 17:08:55 +0530

    change

--- wp-content/themes/twentynineteen/test.php
+++ wp-content/themes/twentynineteen/test.php
@@ -1,6 +1,5 @@
 <?php
-require_once get_stylesheet_directory().'page.php';
-$a = "test";
+include "test";
 $name = filter_input( INPUT_POST, 'name', FILTER_SANITIZE_STRING );
 ?>
 <a href="<?php echo esc_url($name);?>">test</a>
