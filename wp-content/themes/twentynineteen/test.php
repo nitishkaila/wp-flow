b950d6f67949f334572ec38a0af29fb5a3897b8f
md054-Nitish <nitish.kaila@multidots.com>
2019-05-23 17:48:35 +0530

    change

--- wp-content/themes/twentynineteen/test.php
+++ wp-content/themes/twentynineteen/test.php
@@ -1,8 +1,13 @@
 <?php
 
 //$abc = extract($name);
+$a = 1;
+$b = 2;
 $name = filter_input( INPUT_POST, 'name', FILTER_SANITIZE_STRING );
 ?>
 <a href="<?php echo esc_url($name);?>">test</a>
 <?php
+if ( $a == $b ) {
+    echo "sdfsdf";
+}
 
